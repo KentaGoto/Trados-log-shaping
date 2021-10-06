f3cb43e9bd8fdcc4f88f05ac02a7edfa8e30b1d9
KentaGoto <gotokentag@gmail.com>
2021-10-06 13:45:50 +0900

    2nd

--- Trados-log-shaping.py
+++ Trados-log-shaping.py
@@ -5,6 +5,66 @@ import shutil
 import os
 
 
+def inputExcel(countT, count, ws, s):
+    # 文字数カウントを処理する
+    if count == 1:
+        print('file -> ' + s)
+        a = 'A' + countStr
+        ws[a].value = s
+    elif count == 2:
+        print('locked -> ' + s)
+        b = 'B' + countStr
+        ws[b].value = int(s)
+    elif count == 3:
+        print('perfectMatch -> ' + s)
+        c = 'C' + countStr
+        ws[c].value = int(s)
+    elif count == 4:
+        print('contextMatch -> ' + s)
+        d = 'D' + countStr
+        ws[d].value = int(s)
+    elif count == 5:
+        print('crossFileRepetitions -> ' + s)
+        e = 'E' + countStr
+        ws[e].value = int(s)
+    elif count == 6:
+        print('repeated -> ' + s)
+        f = 'F' + countStr
+        ws[f].value = int(s)
+    elif count == 7:
+        print('match100 -> ' + s)
+        g = 'G' + countStr
+        ws[g].value = int(s)
+    elif count == 8:
+        print('match99_95 -> ' + s)
+        h = 'H' + countStr
+        ws[h].value = int(s)
+    elif count == 9:
+        print('match94_85 -> ' + s)
+        i = 'I' + countStr
+        ws[i].value = int(s)
+    elif count == 10:
+        print('mat84_75 -> ' + s)
+        j = 'J' + countStr
+        ws[j].value = int(s)
+    elif count == 11:
+        print('match74_50 -> ' + s)
+        k = 'K' + countStr
+        ws[k].value = int(s)
+    elif count == 12:
+        print('new -> ' + s)
+        l = 'L' + countStr
+        ws[l].value = int(s)
+    elif count == 13:
+        print('totalCharacters -> ' + s)
+        m = 'M' + countStr
+        ws[m].value = int(s)
+
+    if count <= 12:
+        count += 1
+    elif count == 13:
+        count = 1
+
 if __name__ == '__main__':
     s = input("File: ")
     log_file = s.strip('\"')
@@ -41,59 +101,7 @@ if __name__ == '__main__':
 
             countStr = str(countT)
 
-            # 文字数カウントを処理する
-            if count == 1:
-                print('file -> ' + s)
-                a = 'A' + countStr
-                ws[a].value = s
-            elif count == 2:
-                print('locked -> ' + s)
-                b = 'B' + countStr
-                ws[b].value = int(s)
-            elif count == 3:
-                print('perfectMatch -> ' + s)
-                c = 'C' + countStr
-                ws[c].value = int(s)
-            elif count == 4:
-                print('contextMatch -> ' + s)
-                d = 'D' + countStr
-                ws[d].value = int(s)
-            elif count == 5:
-                print('crossFileRepetitions -> ' + s)
-                e = 'E' + countStr
-                ws[e].value = int(s)
-            elif count == 6:
-                print('repeated -> ' + s)
-                f = 'F' + countStr
-                ws[f].value = int(s)
-            elif count == 7:
-                print('match100 -> ' + s)
-                g = 'G' + countStr
-                ws[g].value = int(s)
-            elif count == 8:
-                print('match99_95 -> ' + s)
-                h = 'H' + countStr
-                ws[h].value = int(s)
-            elif count == 9:
-                print('match94_85 -> ' + s)
-                i = 'I' + countStr
-                ws[i].value = int(s)
-            elif count == 10:
-                print('mat84_75 -> ' + s)
-                j = 'J' + countStr
-                ws[j].value = int(s)
-            elif count == 11:
-                print('match74_50 -> ' + s)
-                k = 'K' + countStr
-                ws[k].value = int(s)
-            elif count == 12:
-                print('new -> ' + s)
-                l = 'L' + countStr
-                ws[l].value = int(s)
-            elif count == 13:
-                print('totalCharacters -> ' + s)
-                m = 'M' + countStr
-                ws[m].value = int(s)
+            inputExcel(countT, count, ws, s) # エクセルに入力する
 
             if count <= 12:
                 count += 1
